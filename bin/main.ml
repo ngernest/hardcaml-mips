5ad6e8990d1e01450f90c68c04fad3d1a3f4a756
Ernest Ng <eyn5@cornell.edu>
2025-06-28 16:59:46 -0400

    formatting

--- bin/main.ml
+++ bin/main.ml
@@ -2,5 +2,8 @@ open Hardcaml
 open Mips
 module MipsCircuit = Circuit.With_interface (Datapath.I) (Datapath.O)
 
-let circuit = MipsCircuit.create_exn Datapath.create ~name:"datapath"
+let circuit : Circuit.t =
+  MipsCircuit.create_exn Datapath.create ~name:"datapath"
+
+(* Produces the equivalent Verilog circuit *)
 let () = Rtl.print Verilog circuit
--- test/test_datapath.ml
+++ test/test_datapath.ml
@@ -2,20 +2,20 @@ open Hardcaml
 open Hardcaml_waveterm
 open Mips.Datapath
 
-(* Adapted from https://blog.janestreet.com/using-ascii-waveforms-to-test-hardware-designs/ *)
+(* Adapted from
+   https://blog.janestreet.com/using-ascii-waveforms-to-test-hardware-designs/ *)
 
-(* Create a simulation module by passing the input/output
-   modules to a functor *)
+(* Create a simulation module by passing the input/output modules to a
+   functor *)
 module Simulator = Cyclesim.With_interface (I) (O)
 
 let testbench () : Waveform.t =
   let sim = Simulator.create create in
   let waves, sim = Waveform.create sim in
   let inputs = Cyclesim.inputs sim in
-  let step ~suffix =
+  let step ~(suffix : int) : unit =
     inputs.suffix := if suffix = 1 then Bits.vdd else Bits.gnd;
-    Cyclesim.cycle sim
-  in
+    Cyclesim.cycle sim in
   step ~suffix:0;
   step ~suffix:1;
   step ~suffix:1;
